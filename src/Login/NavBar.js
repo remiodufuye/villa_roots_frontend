2291f0933535bf78847725d10f8e55f76a03d7ef
github username <github email address>
2020-07-26 20:34:31 -0400

    changes made

--- src/Login/NavBar.js
+++ src/Login/NavBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import { Header, Icon, Sticky } from "semantic-ui-react";
-import logo1 from "../images/VillaRoots2.jpeg";
+import { Image } from "semantic-ui-react";
+// import logo1 from "../images/VillaRoots2.jpeg";
 
 class NavBar extends Component {
   render() {
-    return <div> {logo1}</div>;
+    return (
+      <div>
+        <Image src="../images/VillaRoots2.jpeg" />
+      </div>
+    );
   }
 }
 
